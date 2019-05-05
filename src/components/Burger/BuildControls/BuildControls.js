94a3509fc76ce5e081eee4a9ef4df0c0a54ec5bd
Wesley Nooner <wesleynooner@gmail.com>
2019-05-05 18:52:11 -0500

    added favicon

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,18 +13,18 @@ const buildControls = (props) => (
     <div className={classes.BuildControls}>
         <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-            <BuildControl 
-            key={ctrl.label}
-            label={ctrl.label}
-            added={()=>props.ingredientAdded(ctrl.type)}
-            removed={()=>props.ingredientRemoved(ctrl.type)}
-            disabled={props.disabled[ctrl.type]} />
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
         ))}
         <div>
-            <button 
-            className={classes.OrderButton} 
-            disabled={!props.purchasable}
-            onClick={props.ordered}>{props.isAuth ? 'ORDER NOW' : 'SIGN UP TO ORDER'}</button>
+            <button
+                className={classes.OrderButton}
+                disabled={!props.purchasable}
+                onClick={props.ordered}>{props.isAuth ? 'ORDER NOW' : 'SIGN UP TO ORDER'}</button>
         </div>
     </div>
 )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import './index.css';
@@ -27,9 +27,9 @@ const store = createStore(
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter basename="/">
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 )
 
