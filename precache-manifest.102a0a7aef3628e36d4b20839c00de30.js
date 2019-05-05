d4fbda800420e07b68f6459035d84deeab340016
Wesley Nooner <wesleynooner@gmail.com>
2019-05-05 17:08:25 -0500

    Updates

---
+++ precache-manifest.102a0a7aef3628e36d4b20839c00de30.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7979ecd6e8fea991b75d1f320536c6e",
+    "url": "/myBurger/index.html"
+  },
+  {
+    "revision": "9e7428339973a4fe74aa",
+    "url": "/myBurger/static/css/3.7cfa4261.chunk.css"
+  },
+  {
+    "revision": "864e117c9b9b63f60de4",
+    "url": "/myBurger/static/css/4.a94e6bf5.chunk.css"
+  },
+  {
+    "revision": "ecaf05a68512b5ed170d",
+    "url": "/myBurger/static/css/5.e2402d30.chunk.css"
+  },
+  {
+    "revision": "cd0a7aea77821586bb4c",
+    "url": "/myBurger/static/css/main.17c1f104.chunk.css"
+  },
+  {
+    "revision": "f9814b31175a121b4aaf",
+    "url": "/myBurger/static/js/2.142033a6.chunk.js"
+  },
+  {
+    "revision": "9e7428339973a4fe74aa",
+    "url": "/myBurger/static/js/3.51b6b8ba.chunk.js"
+  },
+  {
+    "revision": "864e117c9b9b63f60de4",
+    "url": "/myBurger/static/js/4.9c89958c.chunk.js"
+  },
+  {
+    "revision": "ecaf05a68512b5ed170d",
+    "url": "/myBurger/static/js/5.a1bfffac.chunk.js"
+  },
+  {
+    "revision": "cd0a7aea77821586bb4c",
+    "url": "/myBurger/static/js/main.ea3a41ed.chunk.js"
+  },
+  {
+    "revision": "74a8dfd2b9fca27f70e1",
+    "url": "/myBurger/static/js/runtime~main.f784e4ef.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/myBurger/static/media/burger-logo.b8503d26.png"
+  }
+]);
